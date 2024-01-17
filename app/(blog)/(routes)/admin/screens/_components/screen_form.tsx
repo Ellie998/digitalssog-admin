91040ab33c5403e9032546d6f2fc73f92e2d1a19
Ellie <dpfla9144@naver.com>
2024-01-17 18:40:45 +0900

    add screens table

---
+++ app/(blog)/(routes)/admin/screens/_components/screen_form.tsx
@@ -0,0 +1,38 @@
+import ScreenTable from './screen-table';
+
+const ScreenForm = async () => {
+  const screenData: Array<{
+    id: string;
+    name: string;
+    appName: string;
+    version: string;
+    created_date: string;
+    updated_date: string;
+  }> = [
+    {
+      id: 'dks-23123-dasmlfsm',
+      name: '스크린1',
+      appName: '기본',
+      version: '0.1.2',
+      created_date: '2023.01.23',
+      updated_date: '2023.10.23',
+    },
+  ];
+
+  return (
+    <div className="w-full shadow-md">
+      <ScreenTable
+        data={screenData?.map((data) => ({
+          id: data.id,
+          name: data.name,
+          appName: data.appName,
+          version: data.version,
+          created_date: data.created_date,
+          updated_date: data.updated_date,
+        }))}
+      />
+    </div>
+  );
+};
+
+export default ScreenForm;
