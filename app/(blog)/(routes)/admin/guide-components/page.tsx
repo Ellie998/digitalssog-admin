75e7bea674740b7b78762d2ad83dd308832f40bc
Ellie <dpfla9144@naver.com>
2024-02-17 20:27:12 +0900

    DELETE : blog folder

--- app/(blog)/(routes)/admin/guide-components/page.tsx
+++
@@ -1,9 +0,0 @@
-const GuideComponentsPage = () => {
-  return (
-    <div>
-      <h1 className="py-6 text-2xl font-bold">Guide Components Page</h1>
-    </div>
-  );
-};
-
-export default GuideComponentsPage;
--- app/(blog)/(routes)/admin/layout.tsx
+++
@@ -1,5 +0,0 @@
-const AdminLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="w-full h-full px-10 py-10">{children}</div>;
-};
-
-export default AdminLayout;
